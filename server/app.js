079e3e7c04d695105bd137418241aa776bdc1283
DanielAdek <maildaniel.me1@gmail.com>
2018-11-12 00:14:40 +0100

    chore (setup): Setup project
    - setup express server
    - setup databse connection
    
    [Finishes #161867651]

---
+++ server/app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import jsend from 'jsend';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import handleRoutes from './routes/helpers';
+import connectString from './config/config';
+
+dotenv.config();
+const app = express();
+
+const db = connectString.mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log('Mongodb connected!');
+  })
+  .catch(err =>  console.log(`error ${err.message}`));
+
+const urlencoded = bodyParser.urlencoded({
+  extended: false
+});
+const json = bodyParser.json();
+const port = parseInt(process.env.PORT, 10);
+
+// USE CORS TO AVOID CROSS ORIGIN CONFLICT
+app.use(cors());
+
+
+app.use(jsend.middleware);
+app.use(urlencoded);
+app.use(json);
+app.use(jsend.middleware);
+
+
+// Use user routes
+app.use('/', handleRoutes);
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
+
+export default app;
